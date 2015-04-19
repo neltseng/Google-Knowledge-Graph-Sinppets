f7663f9fe2dde20972405dece5bb159ec442024a
Nel <nel@tseng.cc>
2015-04-20 07:36:56 +0800

    Update functions.min.php

--- functions.min.php
+++ functions.min.php
@@ -1,3 +1,5 @@
+
+// 企業用 Google Knowledge Graph 精簡版
 function cwp_google_knowledge_graph() {
 	if (is_front_page()) {
 		echo '<script type="application/ld+json">{"@context":"http://schema.org","@type":"Organization","name":"cloudwp","url":"https://cloudwp.pro","logo":"https://assets.cloudwp.pro/images/logo-gkg.png","contactPoint":[{"@type":"ContactPoint","telephone":"+886-3-5220-911","contactType":"customer service"}],"sameAs":["https://www.facebook.com/CloudWP","https://plus.google.com/+CloudwpPro","https://twitter.com/cloudwppro","https://www.linkedin.com/company/cloudwp"]}</script>';
